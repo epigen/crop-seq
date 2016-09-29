903f1791e53ebe11ad1a8e54535ebf4808432a8d
jklughammer <jklughammer@cemm.oeaw.ac.at>
2016-09-29 13:21:55 +0200

    change to pdf

--- src/CROP-mixingExperiments.R
+++ src/CROP-mixingExperiments.R
@@ -20,14 +20,14 @@ dge=fread(file.path(data_dir,"digital_expression.500genes.tsv"))
 duplUMIs=umiPerGene[Num_Obs>1,.N,by="Cell Barcode"]
 duplUMIs[,N_topped:=ifelse(N>1000,1000,N),]
 duplScore=nrow(umiPerGene[Num_Obs>1])/nrow(umiPerGene)
-svg(paste0(out_dir,"/duplUMIs_per_cell_hist.svg"),height=6,width=6)
+pdf(paste0(out_dir,"/duplUMIs_per_cell_hist.pdf"),height=6,width=6)
 ggplot(duplUMIs,aes(x=log10(N)))+geom_histogram(bins=40,col="black",fill="grey",size=0.2)+xlab("Number of duplicate UMIs (log10)")+ylab("Number of cells")+ggtitle(paste0("Duplication rate: ",round(duplScore,3)))
 dev.off()
 
 
 #plot percentage of duplicate UMIs per cell
 percent_dup=umiPerGene[,.N,by=c("Num_Obs","Cell Barcode")][,list(percent_dupl=sum(N[Num_Obs>1]/sum(N)*100)),by="Cell Barcode"]
-svg(paste0(out_dir,"/duplUMIs_percent_cell_hist.svg"),height=6,width=6)
+pdf(paste0(out_dir,"/duplUMIs_percent_cell_hist.pdf"),height=6,width=6)
 ggplot(percent_dup,aes(x=percent_dupl))+geom_histogram(bins=50,col="black",fill="grey",size=0.2)+xlab("Percent duplicate UMIs")+ylab("Number of cells")+ggtitle(paste0("Duplication rate: ",round(duplScore,3)))
 dev.off()
 
@@ -54,12 +54,12 @@ double_frac=(nrow(dge_wide_mean[percent_g1.MOUSE>10&percent_g1.MOUSE<90])*2)/nro
 
 #ratio plots
 dge_long_mean[,cell_barcode:=factor(cell_barcode,levels=unique(cell_barcode[order(ratio_mh_g1)])),]
-svg(paste0(out_dir,"/species_ratio_g1.svg"),height=7,width=5)
+pdf(paste0(out_dir,"/species_ratio_g1.pdf"),height=7,width=5)
 ggplot(dge_long_mean[percent_g1!=0], aes(x=cell_barcode,y=percent_g1,fill=species,alpha=log10(total_g1)))+geom_bar(position="stack",stat="identity",col="transparent")+ggtitle(paste0("doublet-estimate: ",round(double_frac,3)*100,"%"))+theme(axis.text.y = element_blank(),panel.grid.major = element_blank(), panel.grid.minor = element_blank())+coord_flip()+xlab("Cells")+ylab("% of total detected genes")
 dev.off()
 
 #mouse vs. human
-svg(paste0(out_dir,"/species_cor_g1.svg"),height=6,width=5)
+pdf(paste0(out_dir,"/species_cor_g1.pdf"),height=6,width=5)
 ggplot(dge_wide_mean, aes(x=log10(g1.MOUSE),y=log10(g1.HUMAN),fill=species_dom.MOUSE,col=doublet))+geom_abline(slope=1)+geom_point(shape=21,alpha=0.4,size=2)+theme(legend.position="bottom")+ggtitle(paste0("doublet-estimate: ",round(double_frac,3)*100,"%"))+xlim(c(0,4))+ylim(c(0,4))+scale_color_manual(values=c("TRUE"="black","FALSE"="grey"))
 dev.off()
 
