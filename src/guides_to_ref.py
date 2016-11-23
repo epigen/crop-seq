35a2c5c6018eb62da116c3b0723fb84092014a7a
Andre Rendeiro <afrendeiro@gmail.com>
2016-11-23 21:55:57 +0100

    add support for several gRNA libraries when making transcriptome indices

--- src/guides_to_ref.py
+++ src/guides_to_ref.py
@@ -54,74 +54,79 @@ gtf_template = """{chrom}_chrom\thavana\tgene\t1\t{length}\t.\t+\t.\tgene_id "{i
 {chrom}_chrom\thavana\texon\t1\t{length}\t.\t+\t.\tgene_id "{id}_gene"; transcript_id "{id}_transcript"; exon_number "1"; gene_name "{id}_gene"; gene_source "ensembl_havana"; gene_biotype "lincRNA"; transcript_name "{id}_transcript"; transcript_source "havana"; exon_id "{id}_exon";
 """
 
-prj = Project(os.path.join("metadata", "config.yaml"))
+# initialize project
+prj = Project(os.path.join("metadata", "config.separate.yaml"))
+
+# read in guide annotation
+annotation = pd.read_csv(os.path.join("metadata", "guide_annotation.csv"))
 
 for genome in prj.genomes.__dict__.keys():
-    genome = getattr(prj.genomes, "human")
     output_dir = os.path.join(prj.paths.output_dir, "spiked_genomes")
+    for library in annotation['library'].drop_duplicates():
 
-    spiked_dir = os.path.join(output_dir, genome)
+        library_annotation = annotation[annotation['library'] == library]
 
-    for _dir in [prj.paths.output_dir, output_dir, spiked_dir]:
-        if not os.path.exists(_dir):
-            os.makedirs(_dir)
+        genome_ref = getattr(prj.genomes, genome)
 
-    output_fasta = os.path.join(output_dir, "gRNA_spikes.fa")
-    output_gtf = os.path.join(output_dir, "gRNA_spikes.gtf")
+        spiked_dir = os.path.join(output_dir, genome_ref) + "_" + library
 
-    # read in guide annotation
-    annotation = os.path.join(prj.paths.output_dir, "metadata", "guide_annotation.csv")
-    df = pd.read_csv(annotation)
+        for _dir in [output_dir, spiked_dir]:
+            if not os.path.exists(_dir):
+                os.makedirs(_dir)
 
-    write_annotation(df, prj.config, output_fasta, output_gtf)
+        output_fasta = os.path.join(spiked_dir, "gRNA_spikes.fa")
+        output_gtf = os.path.join(spiked_dir, "gRNA_spikes.gtf")
 
-    # Make STAR index and supporting files
-    if genome == "hg38_spiked":
-        # Get fasta genome
-        os.system(
-            "wget -O {} ftp://ftp.ensembl.org/pub/release-77/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
-            .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz")))
-        os.system("gzip -d {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz")))
-        # Get gtf annotation
-        os.system(
-            "wget -O {} ftp://ftp.ensembl.org/pub/release-77/gtf/homo_sapiens/Homo_sapiens.GRCh38.77.gtf.gz"
-            .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf.gz")))
-        os.system("gzip -d {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf.gz")))
+        # write gRNA library annotation
+        write_annotation(library_annotation, prj.config, output_fasta, output_gtf)
 
-        # Add extra chromosomes (CROP-seq constructs) to genome
-        os.system(
-            "cat {} {} > {}"
-            .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa"), output_fasta, os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa")))
-        os.system(
-            "cat {} {} > {}"
-            .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf"), output_gtf, os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf")))
+        # Make STAR index and supporting files
+        if genome_ref == "hg38_spiked":
+            # Get fasta genome
+            os.system(
+                "wget -O {} ftp://ftp.ensembl.org/pub/release-77/fasta/homo_sapiens/dna/Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz"
+                .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz")))
+            os.system("gzip -d {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa.gz")))
+            # Get gtf annotation
+            os.system(
+                "wget -O {} ftp://ftp.ensembl.org/pub/release-77/gtf/homo_sapiens/Homo_sapiens.GRCh38.77.gtf.gz"
+                .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf.gz")))
+            os.system("gzip -d {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf.gz")))
 
-        # Build STAR index (contruct + spiked with gRNAs)
-        cmd = "srun --mem 80000 -p develop /cm/shared/apps/star/2.4.2a/STAR"
-        cmd += " --runThreadN 8"
-        cmd += " --runMode genomeGenerate"
-        cmd += " --genomeDir {}".format(spiked_dir)
-        cmd += " --genomeFastaFiles {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa"))
-        cmd += " --sjdbGTFfile {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf"))
-        cmd += " --sjdbOverhang 74"
-        os.system(cmd)
+            # Add extra chromosomes (CROP-seq constructs) to genome
+            os.system(
+                "cat {} {} > {}"
+                .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa"), output_fasta, os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa")))
+            os.system(
+                "cat {} {} > {}"
+                .format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf"), output_gtf, os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf")))
 
-        # Create sequence dictionaries (for piccard)
-        cmd = "srun --mem 80000 -p develop java -Xmx8g -jar /cm/shared/apps/picard-tools/1.140/picard.jar"
-        cmd += " CreateSequenceDictionary"
-        cmd += " REFERENCE={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa"))
-        cmd += " OUTPUT={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.dict"))
-        cmd += " GENOME_ASSEMBLY={}".format(genome)
-        cmd += " SPECIES=human"
-        os.system(cmd)
+            # Build STAR index (contruct + spiked with gRNAs)
+            cmd = "srun --mem 80000 -p develop /cm/shared/apps/star/2.4.2a/STAR"
+            cmd += " --runThreadN 8"
+            cmd += " --runMode genomeGenerate"
+            cmd += " --genomeDir {}".format(spiked_dir)
+            cmd += " --genomeFastaFiles {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa"))
+            cmd += " --sjdbGTFfile {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf"))
+            cmd += " --sjdbOverhang 74"
+            os.system(cmd)
 
-        # Create reflat files
-        cmd = "~/Drop-seq_tools-1.12/ConvertToRefFlat"
-        cmd += " SEQUENCE_DICTIONARY={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.dict"))
-        cmd += " ANNOTATIONS_FILE= {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf"))
-        cmd += " OUTPUT={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.refFlat"))
-        os.system(cmd)
+            # Create sequence dictionaries (for piccard)
+            cmd = "srun --mem 80000 -p develop java -Xmx8g -jar /cm/shared/apps/picard-tools/1.140/picard.jar"
+            cmd += " CreateSequenceDictionary"
+            cmd += " REFERENCE={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.fa"))
+            cmd += " OUTPUT={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.dict"))
+            cmd += " GENOME_ASSEMBLY={}".format(genome)
+            cmd += " SPECIES=human"
+            os.system(cmd)
 
-        # Remove vanilla genome
-        os.remove(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa"))
-        os.remove(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf"))
+            # Create reflat files
+            cmd = "~/Drop-seq_tools-1.12/ConvertToRefFlat"
+            cmd += " SEQUENCE_DICTIONARY={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.dict"))
+            cmd += " ANNOTATIONS_FILE= {}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.spiked.gtf"))
+            cmd += " OUTPUT={}".format(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.spiked.refFlat"))
+            os.system(cmd)
+
+            # Remove vanilla genome
+            os.remove(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.dna.primary_assembly.fa"))
+            os.remove(os.path.join(spiked_dir, "Homo_sapiens.GRCh38.77.gtf"))
